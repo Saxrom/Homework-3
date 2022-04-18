d63e477d678b6c1f010635f7f453e60eca52fd00
Shahrom <shahromkr@code.edu.az>
2022-04-18 23:55:38 +0400

    Added homework№3

---
+++ Homework№3/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 10, 8, 49, 17, 5 };
+
+
+            Console.WriteLine(Maxnumber(array));
+        }
+
+        public static int Maxnumber(int[] array)
+        {
+            int max = array[0];
+
+            foreach (var item in array)
+            {
+                if (max < item)
+                {
+                    max = item;
+                }
+                
+            }
+            return max;
+        }
+           
+    }
+}
